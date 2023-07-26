227dca983fb54c29aaa1e612fff48479d148925c
RodOP91 <rodrigo.op1791@gmail.com>
2023-07-26 15:52:35 -0600

    feat: added PriceList and ProductPriceList models

--- catalog/models.py
+++ catalog/models.py
@@ -9,3 +9,15 @@ class Product(models.Model):
     
     def __str__(self):
         return f"{self.name} ({self.brand})"
+
+class PriceList(models.Model):
+    name= models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+
+class ProductPriceList(models.Model):
+    price_list_id = models.ForeignKey(PriceList, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=3)
+    
+    class Meta:
+        unique_together = ('product_id', 'price_list_id')
